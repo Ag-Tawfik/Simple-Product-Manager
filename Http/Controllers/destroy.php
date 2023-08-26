9033db2dfbc94ad8c1da38cf54d30d8405967d19
AgTaoufik <Ag.taoufik@gmail.com>
2023-08-26 13:57:35 +0300

    Fix Some Errors

--- Http/Controllers/destroy.php
+++ Http/Controllers/destroy.php
@@ -7,10 +7,12 @@ $db = App::resolve(Database::class);
 
 if (isset($_POST['delete'])) {
     $ids = $_POST['delete'];
-    foreach ($ids as $id) {
-        $db->query('DELETE FROM products WHERE id = :id', [
-            'id' => $id
-        ]);
+    if (is_array($ids)) {
+        foreach ($ids as $id) {
+            $db->query('DELETE FROM products WHERE id = :id', [
+                'id' => $id
+            ]);
+        }
     }
 }
 
